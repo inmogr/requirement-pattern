95839614d03d3c8191db4f49b65aff3bff440f64
Morhaf Shamia <inmogr@users.noreply.github.com>
2018-07-14 03:37:21 +0800

    Update spl domain.sql

--- sql/spl domain.sql
+++ sql/spl domain.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `spl_domain` (
   `spl_domain_name` varchar(45) NOT NULL,
-  `goal` varchar(45) NOT NULL,
-  `pattern_description` varchar(45) NOT NULL,
-  `date_created` varchar(45) NOT NULL,
-  `domain_author` varchar(45) NOT NULL,
+  `goal` longtext NOT NULL,
+  `pattern_description` longtext NOT NULL,
+  `date_created` longtext NOT NULL,
+  `domain_author` longtext NOT NULL,
   PRIMARY KEY (`spl_domain_name`),
   UNIQUE KEY `spl_domain_name_UNIQUE` (`spl_domain_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='	';
