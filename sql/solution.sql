7f8758696f48999f72929eeb1f044aa9b504e69d
Morhaf Shamia <inmogr@users.noreply.github.com>
2018-07-14 03:37:04 +0800

    Update solution.sql

--- sql/solution.sql
+++ sql/solution.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `solution` (
-  `solution_id` int(20) NOT NULL,
+  `solution_id` varchar(10) NOT NULL,
   `solution_name` longtext NOT NULL,
-  `pattern_id` int(20) NOT NULL,
+  `pattern_id` varchar(10) NOT NULL,
   `goal` longtext NOT NULL,
   `description` longtext NOT NULL,
   `variability_model_vm_description` longtext NOT NULL,
